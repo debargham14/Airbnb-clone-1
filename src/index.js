0a8fb2fd68164ffe661750b0d630c43e928de4a1
debargham14 <debargham14@gmail.com>
2021-11-03 00:31:30 +0530

    git config changes added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//deployment testing command added  
 ReactDOM.render(
   <React.StrictMode>
     <App />
